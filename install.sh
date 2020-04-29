8230de6f3690ecc092df962511a5a2b8708f2432
Mazhar <1526155-Mazharul-Hossain@users.noreply.gitlab.com>
2020-04-29 01:10:22 -0500

    solved Vagrant error for NFD installation

--- install.sh
+++ install.sh
@@ -60,29 +60,29 @@
 #   without specific, written prior permission.
 
 test -e /etc/debian_version && DIST="Debian"
-grep Ubuntu /etc/lsb-release &> /dev/null && DIST="Ubuntu"
+grep Ubuntu /etc/lsb-release &>/dev/null && DIST="Ubuntu"
 
 if [[ $DIST == Ubuntu || $DIST == Debian ]]; then
-    update='sudo apt-get update'
-    install='sudo apt-get -y install'
-    remove='sudo apt-get -y remove'
-    pkginst='sudo dpkg -i'
-    # Prereqs for this script
-    if ! which lsb_release &> /dev/null; then
-        $install lsb-release
-    fi
+  update='sudo apt-get update'
+  install='sudo apt-get -y install'
+  remove='sudo apt-get -y remove'
+  pkginst='sudo dpkg -i'
+  # Prereqs for this script
+  if ! which lsb_release &>/dev/null; then
+    $install lsb-release
+  fi
 fi
 
 test -e /etc/fedora-release && DIST="Fedora"
 if [[ $DIST == Fedora ]]; then
-    update='sudo yum update'
-    install='sudo yum -y install'
-    remove='sudo yum -y erase'
-    pkginst='sudo rpm -ivh'
-    # Prereqs for this script
-    if ! which lsb_release &> /dev/null; then
-        $install redhat-lsb-core
-    fi
+  update='sudo yum update'
+  install='sudo yum -y install'
+  remove='sudo yum -y erase'
+  pkginst='sudo rpm -ivh'
+  # Prereqs for this script
+  if ! which lsb_release &>/dev/null; then
+    $install redhat-lsb-core
+  fi
 fi
 
 NDN_SRC="ndn-src"
@@ -97,329 +97,331 @@ NLSR_VERSION="master"
 NDN_TOOLS_VERSION="master"
 
 if [ $SUDO_USER ]; then
-    REAL_USER=$SUDO_USER
+  REAL_USER=$SUDO_USER
 else
-    REAL_USER=$(whoami)
+  REAL_USER=$(whoami)
 fi
 
-function patchDummy {
-    git -C $NDN_SRC/ndn-cxx apply $(pwd)/patches/ndn-cxx-dummy-keychain-from-ndnsim.patch
-    if [[ "$?" -ne 0 ]]; then
-        echo "Patch might already be applied"
-    fi
+function patchDummy() {
+  git -C $NDN_SRC/ndn-cxx apply $(pwd)/patches/ndn-cxx-dummy-keychain-from-ndnsim.patch
+  if [[ "$?" -ne 0 ]]; then
+    echo "Patch might already be applied"
+  fi
 }
 
-function quiet_install {
-    if [[ $DIST == Ubuntu || $DIST == Debian ]]; then
-        update='sudo DEBIAN_FRONTEND=noninteractive apt-get update'
-        install='sudo DEBIAN_FRONTEND=noninteractive apt-get -y install'
-        remove='sudo DEBIAN_FRONTEND=noninteractive apt-get -y remove'
+function quiet_install() {
+  if [[ $DIST == Ubuntu || $DIST == Debian ]]; then
+    update='sudo DEBIAN_FRONTEND=noninteractive apt-get update'
+    install='sudo DEBIAN_FRONTEND=noninteractive apt-get -y install'
+    remove='sudo DEBIAN_FRONTEND=noninteractive apt-get -y remove'
 
-        echo "wireshark-common wireshark-common/install-setuid boolean false" | sudo debconf-set-selections
-    fi
+    echo "wireshark-common wireshark-common/install-setuid boolean false" | sudo debconf-set-selections
+  fi
 }
 
-function ndn_install {
-    mkdir -p $NDN_SRC
-    name=$1
-    version=$2
-    wafOptions=$3
+function ndn_install() {
+  mkdir -p $NDN_SRC
+  name=$1
+  version=$2
+  wafOptions=$3
 
-    if [[ $version == "master" ]]; then
-        if [[ -d "$NDN_SRC/$name" ]]; then
-            pushd $NDN_SRC/$name
-            git checkout master
-        else
-            git clone --depth 1 $NDN_GITHUB/$name $NDN_SRC/$name
-            pushd $NDN_SRC/$name
-        fi
+  if [[ $version == "master" ]]; then
+    if [[ -d "$NDN_SRC/$name" ]]; then
+      pushd $NDN_SRC/$name
+      git checkout master
     else
-        if [[ -d $NDN_SRC/$name ]]; then
-            pushd $NDN_SRC/$name
-            if [[ $(git rev-parse --is-shallow-repository) == "true" ]]; then
-                git fetch --unshallow
-                git fetch --all
-            fi
-        else
-            git clone $NDN_GITHUB/$name $NDN_SRC/$name
-            pushd $NDN_SRC/$name
-        fi
-        git checkout $version -b version-$version || git checkout version-$version
+      git clone --depth 1 $NDN_GITHUB/$name $NDN_SRC/$name
+      pushd $NDN_SRC/$name
     fi
+  else
+    if [[ -d $NDN_SRC/$name ]]; then
+      pushd $NDN_SRC/$name
+      if [[ $(git rev-parse --is-shallow-repository) == "true" ]]; then
+        git fetch --unshallow
+        git fetch --all
+      fi
+    else
+      git clone $NDN_GITHUB/$name $NDN_SRC/$name
+      pushd $NDN_SRC/$name
+    fi
+    git checkout $version -b version-$version || git checkout version-$version
+  fi
 
-    # User must use the same python version as root to use ./waf outside of this script
-    sudo -E -u $REAL_USER ./waf configure $wafOptions
-    sudo -E -u $REAL_USER ./waf && sudo ./waf install && sudo ldconfig
-    popd
+  # User must use the same python version as root to use ./waf outside of this script
+  sudo -E -u $REAL_USER ./waf configure $wafOptions
+  sudo -E -u $REAL_USER ./waf && sudo ./waf install && sudo ldconfig
+  popd
 }
 
-# mazhar wrote it as has no idea hot to incorporate his requirement 
-function custom_install {
-    # name=NFD $NFD_VERSION=master --without-websocket
-    NFD_GITHUB="https://github.com/Mazharul-Hossain/NFD.git"
-    mkdir -p $NDN_SRC
-    name=$1
-    version=$2
-    wafOptions=$3
+# mazhar wrote it as has no idea hot to incorporate his requirement
+function custom_install() {
+  # name=NFD $NFD_VERSION=master --without-websocket
+  NFD_GITHUB="https://github.com/Mazharul-Hossain/NFD.git"
+  mkdir -p $NDN_SRC
+  name=$1
+  version=$2
+  wafOptions=$3
 
-    if [[ $version == "master" ]]; then
-        if [[ -d "$NDN_SRC/$name" ]]; then
-            pushd $NDN_SRC/$name
-            git checkout master
-        else
-            git clone --depth 1 $NFD_GITHUB $NDN_SRC/$name
-            pushd $NDN_SRC/$name
-        fi    
+  if [[ $version == "master" ]]; then
+    if [[ -d "$NDN_SRC/$name" ]]; then
+      pushd $NDN_SRC/$name
+      git checkout master
+    else
+      git clone --depth 1 $NFD_GITHUB $NDN_SRC/$name
+      pushd $NDN_SRC/$name
     fi
+  fi
 
-    # https://stackoverflow.com/a/22674820/2049763
-    # Update the user's PYTHONPATH.
-    # Update the user's PYTHONPATH.
-    echo "export PYTHONPATH=\$PYTHONPATH:`pwd`/daemon/fw/IFS-RL" >> ~/.bashrc
-    # Also update root's PYTHONPATH in case of running under sudo.
-    echo "export PYTHONPATH=\$PYTHONPATH:`pwd`/daemon/fw/IFS-RL" | sudo tee -a /root/.bashrc > /dev/null
+  # https://stackoverflow.com/a/22674820/2049763
+  # Update the user's PYTHONPATH.
+  # Update the user's PYTHONPATH.
+  echo "export PYTHONPATH=\$PYTHONPATH:$(pwd)/daemon/fw/IFS-RL" >>~/.bashrc
+  # Also update root's PYTHONPATH in case of running under sudo.
+  echo "export PYTHONPATH=\$PYTHONPATH:$(pwd)/daemon/fw/IFS-RL" | sudo tee -a /root/.bashrc >/dev/null
 
-    echo "export CPLUS_INCLUDE_PATH=/usr/include/python2.7" >> ~/.bashrc
-    # Also update root's PYTHONPATH in case of running under sudo.
-    echo "export CPLUS_INCLUDE_PATH=/usr/include/python2.7" | sudo tee -a /root/.bashrc > /dev/null
+  echo "export CPLUS_INCLUDE_PATH=/usr/include/python2.7" >>~/.bashrc
+  # Also update root's PYTHONPATH in case of running under sudo.
+  echo "export CPLUS_INCLUDE_PATH=/usr/include/python2.7" | sudo tee -a /root/.bashrc >/dev/null
 
-    source ~/.bashrc
-    sudo source /root/.bashrc
+  source ~/.bashrc
+  sudo source /root/.bashrc
 
-    # User must use the same python version as root to use ./waf outside of this script
-    sudo -E -u $REAL_USER ./waf configure $wafOptions
-    sudo -E -u $REAL_USER ./waf && sudo ./waf install && sudo ldconfig
-    popd
+  # User must use the same python version as root to use ./waf outside of this script
+  echo "sudo -E -u $REAL_USER ./waf configure $wafOptions"
+  sudo -E -u $REAL_USER ./waf configure $wafOptions
+
+  echo "sudo -E -u $REAL_USER ./waf && sudo ./waf install && sudo ldconfig"
+  sudo -E -u $REAL_USER ./waf && sudo ./waf install && sudo ldconfig
+  popd
 }
 
-function ndn {
-    if [[ updated != true ]]; then
-        $update
-        updated="true"
-    fi
+function ndn() {
+  if [[ updated != true ]]; then
+    $update
+    updated="true"
+  fi
 
-    if [[ $DIST == Ubuntu || $DIST == Debian ]]; then
-        $install git libsqlite3-dev libboost-all-dev make g++ libssl-dev libpcap-dev pkg-config python-pip
-    fi
+  if [[ $DIST == Ubuntu || $DIST == Debian ]]; then
+    $install git libsqlite3-dev libboost-all-dev make g++ libssl-dev libpcap-dev pkg-config python-pip
+  fi
 
-    if [[ $DIST == Fedora ]]; then
-        $install gcc-c++ sqlite-devel boost-devel openssl-devel libpcap-devel python-pip
-    fi
+  if [[ $DIST == Fedora ]]; then
+    $install gcc-c++ sqlite-devel boost-devel openssl-devel libpcap-devel python-pip
+  fi
 
-    ndn_install ndn-cxx $NDN_CXX_VERSION
-    custom_install NFD $NFD_VERSION --without-websocket
-    ndn_install PSync $PSYNC_VERSION --with-examples
-    ndn_install ChronoSync $CHRONOSYNC_VERSION
-    ndn_install NLSR $NLSR_VERSION
-    ndn_install ndn-tools $NDN_TOOLS_VERSION
-    infoedit
+  ndn_install ndn-cxx $NDN_CXX_VERSION
+  custom_install NFD $NFD_VERSION --without-websocket
+  ndn_install PSync $PSYNC_VERSION --with-examples
+  ndn_install ChronoSync $CHRONOSYNC_VERSION
+  ndn_install NLSR $NLSR_VERSION
+  ndn_install ndn-tools $NDN_TOOLS_VERSION
+  infoedit
 }
 
-function mininet {
-    if [[ updated != true ]]; then
-        $update
-        updated="true"
-    fi
+function mininet() {
+  if [[ updated != true ]]; then
+    $update
+    updated="true"
+  fi
 
-    if [[ $pysetup != true ]]; then
-        pysetup="true"
-    fi
+  if [[ $pysetup != true ]]; then
+    pysetup="true"
+  fi
 
-    git clone --depth 1 https://github.com/mininet/mininet
-    pushd mininet
-    sudo ./util/install.sh -nv
-    popd
+  git clone --depth 1 https://github.com/mininet/mininet
+  pushd mininet
+  sudo ./util/install.sh -nv
+  popd
 }
 
-function infoedit {
-    git clone --depth 1 https://github.com/NDN-Routing/infoedit.git $NDN_SRC/infoedit
-    pushd $NDN_SRC/infoedit
-    rm infoedit
-    sudo make install
-    popd
+function infoedit() {
+  git clone --depth 1 https://github.com/NDN-Routing/infoedit.git $NDN_SRC/infoedit
+  pushd $NDN_SRC/infoedit
+  rm infoedit
+  sudo make install
+  popd
 }
 
-function minindn {
-    $install libigraph0-dev tshark
-    sudo pip install -r requirements.txt
+function minindn() {
+  $install libigraph0-dev tshark
+  sudo pip install -r requirements.txt
 
-    if [[ updated != true ]]; then
-        if [ ! -d "build" ]; then
-            $update
-            updated="true"
-        fi
+  if [[ updated != true ]]; then
+    if [ ! -d "build" ]; then
+      $update
+      updated="true"
     fi
+  fi
 
-    if [[ $pysetup != true ]]; then
-        $install python-setuptools
-        pysetup="true"
-    fi
-    install_dir="/usr/local/etc/mini-ndn/"
+  if [[ $pysetup != true ]]; then
+    $install python-setuptools
+    pysetup="true"
+  fi
+  install_dir="/usr/local/etc/mini-ndn/"
 
-    sudo mkdir -p "$install_dir"
-    sudo cp topologies/default-topology.conf "$install_dir"
-    sudo cp topologies/minindn.caida.conf "$install_dir"
-    sudo cp topologies/minindn.ucla.conf "$install_dir"
-    sudo cp topologies/minindn.testbed.conf "$install_dir"
-    sudo cp topologies/current-testbed.conf "$install_dir"
-    sudo cp topologies/geo_hyperbolic_test.conf "$install_dir"
-    sudo cp topologies/geant.conf "$install_dir"
-    sudo python setup.py develop
+  sudo mkdir -p "$install_dir"
+  sudo cp topologies/default-topology.conf "$install_dir"
+  sudo cp topologies/minindn.caida.conf "$install_dir"
+  sudo cp topologies/minindn.ucla.conf "$install_dir"
+  sudo cp topologies/minindn.testbed.conf "$install_dir"
+  sudo cp topologies/current-testbed.conf "$install_dir"
+  sudo cp topologies/geo_hyperbolic_test.conf "$install_dir"
+  sudo cp topologies/geant.conf "$install_dir"
+  sudo python setup.py develop
 }
 
-function ndn_cpp {
-    if [[ updated != true ]]; then
-        $update
-        updated="true"
-    fi
+function ndn_cpp() {
+  if [[ updated != true ]]; then
+    $update
+    updated="true"
+  fi
 
-    if [[ $DIST == Ubuntu || $DIST == Debian ]]; then
-        $install git build-essential libssl-dev libsqlite3-dev libboost-all-dev libprotobuf-dev protobuf-compiler
-    fi
+  if [[ $DIST == Ubuntu || $DIST == Debian ]]; then
+    $install git build-essential libssl-dev libsqlite3-dev libboost-all-dev libprotobuf-dev protobuf-compiler
+  fi
 
-    if [[ $DIST == Fedora ]]; then
-        printf '\nNDN-CPP does not support Fedora yet.\n'
-        return
-    fi
+  if [[ $DIST == Fedora ]]; then
+    printf '\nNDN-CPP does not support Fedora yet.\n'
+    return
+  fi
 
-    git clone --depth 1 $NDN_GITHUB/ndn-cpp $NDN_SRC/ndn-cpp
-    pushd $NDN_SRC/ndn-cpp
-    ./configure
-    proc=$(nproc)
-    make -j$proc
-    sudo make install
-    sudo ldconfig
-    popd
+  git clone --depth 1 $NDN_GITHUB/ndn-cpp $NDN_SRC/ndn-cpp
+  pushd $NDN_SRC/ndn-cpp
+  ./configure
+  proc=$(nproc)
+  make -j$proc
+  sudo make install
+  sudo ldconfig
+  popd
 }
 
-function pyNDN {
-    if [[ updated != true ]]; then
-        $update
-        updated="true"
-    fi
+function pyNDN() {
+  if [[ updated != true ]]; then
+    $update
+    updated="true"
+  fi
 
-    if [[ $DIST == Ubuntu || $DIST == Debian ]]; then
-        $install git build-essential libssl-dev libffi-dev python-dev python-pip
-    fi
+  if [[ $DIST == Ubuntu || $DIST == Debian ]]; then
+    $install git build-essential libssl-dev libffi-dev python-dev python-pip
+  fi
 
-    if [[ $DIST == Fedora ]]; then
-        printf '\nPyNDN does not support Fedora yet.\n'
-        return
-    fi
+  if [[ $DIST == Fedora ]]; then
+    printf '\nPyNDN does not support Fedora yet.\n'
+    return
+  fi
 
-    sudo pip install cryptography trollius protobuf pytest mock
-    git clone --depth 1 $NDN_GITHUB/PyNDN2 $NDN_SRC/PyNDN2
-    pushd $NDN_SRC/PyNDN2
-    # Update the user's PYTHONPATH.
-    echo "export PYTHONPATH=\$PYTHONPATH:`pwd`/python" >> ~/.bashrc
-    # Also update root's PYTHONPATH in case of running under sudo.
-    echo "export PYTHONPATH=\$PYTHONPATH:`pwd`/python" | sudo tee -a /root/.bashrc > /dev/null
-    popd
+  sudo pip install cryptography trollius protobuf pytest mock
+  git clone --depth 1 $NDN_GITHUB/PyNDN2 $NDN_SRC/PyNDN2
+  pushd $NDN_SRC/PyNDN2
+  # Update the user's PYTHONPATH.
+  echo "export PYTHONPATH=\$PYTHONPATH:$(pwd)/python" >>~/.bashrc
+  # Also update root's PYTHONPATH in case of running under sudo.
+  echo "export PYTHONPATH=\$PYTHONPATH:$(pwd)/python" | sudo tee -a /root/.bashrc >/dev/null
+  popd
 }
 
-function ndn_js {
-    if [[ updated != true ]]; then
-        $update
-        updated="true"
-    fi
+function ndn_js() {
+  if [[ updated != true ]]; then
+    $update
+    updated="true"
+  fi
 
-    if [[ $DIST == Ubuntu || $DIST == Debian ]]; then
-        $install git nodejs npm
-    fi
+  if [[ $DIST == Ubuntu || $DIST == Debian ]]; then
+    $install git nodejs npm
+  fi
 
-    if [[ $DIST == Fedora ]]; then
-        printf '\nNDN-JS does not support Fedora yet.\n'
-        return
-    fi
+  if [[ $DIST == Fedora ]]; then
+    printf '\nNDN-JS does not support Fedora yet.\n'
+    return
+  fi
 
-    sudo ln -fs /usr/bin/nodejs /usr/bin/node
-    sudo npm install -g mocha
-    sudo npm install rsa-keygen sqlite3
-    git clone --depth 1 $NDN_GITHUB/ndn-js $NDN_SRC/ndn-js
+  sudo ln -fs /usr/bin/nodejs /usr/bin/node
+  sudo npm install -g mocha
+  sudo npm install rsa-keygen sqlite3
+  git clone --depth 1 $NDN_GITHUB/ndn-js $NDN_SRC/ndn-js
 }
 
-function jNDN {
-    if [[ updated != true ]]; then
-        $update
-        updated="true"
-    fi
+function jNDN() {
+  if [[ updated != true ]]; then
+    $update
+    updated="true"
+  fi
 
-    if [[ $DIST == Ubuntu || $DIST == Debian ]]; then
-        $install git openjdk-8-jdk maven
-    fi
+  if [[ $DIST == Ubuntu || $DIST == Debian ]]; then
+    $install git openjdk-8-jdk maven
+  fi
 
-    if [[ $DIST == Fedora ]]; then
-        printf '\nNDN-JS does not support Fedora yet.\n'
-        return
-    fi
+  if [[ $DIST == Fedora ]]; then
+    printf '\nNDN-JS does not support Fedora yet.\n'
+    return
+  fi
 
-    git clone --depth 1 $NDN_GITHUB/jndn $NDN_SRC/jndn
-    pushd $NDN_SRC/jndn
-    mvn install
-    popd
+  git clone --depth 1 $NDN_GITHUB/jndn $NDN_SRC/jndn
+  pushd $NDN_SRC/jndn
+  mvn install
+  popd
 }
 
-function commonClientLibraries {
-    ndn_cpp
-    pyNDN
-    ndn_js
-    jNDN
+function commonClientLibraries() {
+  ndn_cpp
+  pyNDN
+  ndn_js
+  jNDN
 }
 
-function buildDocumentation {
-    sphinxInstalled=$(pip show sphinx | wc -l)
-    sphinxRtdInstalled=$(pip show sphinx_rtd_theme | wc -l)
-    if [[ $sphinxInstalled -eq "0" ]]; then
-        pip install sphinx
-    fi
+function buildDocumentation() {
+  sphinxInstalled=$(pip show sphinx | wc -l)
+  sphinxRtdInstalled=$(pip show sphinx_rtd_theme | wc -l)
+  if [[ $sphinxInstalled -eq "0" ]]; then
+    pip install sphinx
+  fi
 
-    if [[ $sphinxRtdInstalled -eq "0" ]]; then
-        pip install sphinx_rtd_theme
-    fi
-    cd docs
-    make clean
-    make html
+  if [[ $sphinxRtdInstalled -eq "0" ]]; then
+    pip install sphinx_rtd_theme
+  fi
+  cd docs
+  make clean
+  make html
 }
 
-function usage {
-    printf '\nUsage: %s [-a]\n\n' $(basename $0) >&2
+function usage() {
+  printf '\nUsage: %s [-a]\n\n' $(basename $0) >&2
 
-    printf 'options:\n' >&2
-    printf -- ' -a: install all the required dependencies\n' >&2
-    printf -- ' -c: install Common Client Libraries\n' >&2
-    printf -- ' -d: build documentation\n' >&2
-    printf -- ' -h: print this (H)elp message\n' >&2
-    printf -- ' -i: install mini-ndn\n' >&2
-    printf -- ' -m: install mininet and dependencies\n' >&2
-    printf -- ' -n: install NDN dependencies of mini-ndn including infoedit\n' >&2
-    printf -- ' -p: patch ndn-cxx with dummy key chain\n' >&2
-    printf -- ' -q: quiet install (must be specified first)\n' >&2
-    exit 2
+  printf 'options:\n' >&2
+  printf -- ' -a: install all the required dependencies\n' >&2
+  printf -- ' -c: install Common Client Libraries\n' >&2
+  printf -- ' -d: build documentation\n' >&2
+  printf -- ' -h: print this (H)elp message\n' >&2
+  printf -- ' -i: install mini-ndn\n' >&2
+  printf -- ' -m: install mininet and dependencies\n' >&2
+  printf -- ' -n: install NDN dependencies of mini-ndn including infoedit\n' >&2
+  printf -- ' -p: patch ndn-cxx with dummy key chain\n' >&2
+  printf -- ' -q: quiet install (must be specified first)\n' >&2
+  exit 2
 }
 
 if [[ $# -eq 0 ]]; then
-    usage
+  usage
 else
-    while getopts 'acdhimnpq' OPTION
-    do
-        case $OPTION in
-        a)
-        ndn
-        mininet
-        minindn
-        commonClientLibraries
-        break
-        ;;
-        c)    commonClientLibraries;;
-        d)    buildDocumentation;;
-        h)    usage;;
-        i)    minindn;;
-        m)    mininet;;
-        n)    ndn;;
-        p)    patchDummy;;
-        q)    quiet_install;;
-        ?)    usage;;
-        esac
-    done
-    shift $(($OPTIND - 1))
+  while getopts 'acdhimnpq' OPTION; do
+    case $OPTION in
+    a)
+      ndn
+      mininet
+      minindn
+      commonClientLibraries
+      break
+      ;;
+    c) commonClientLibraries ;;
+    d) buildDocumentation ;;
+    h) usage ;;
+    i) minindn ;;
+    m) mininet ;;
+    n) ndn ;;
+    p) patchDummy ;;
+    q) quiet_install ;;
+    ?) usage ;;
+    esac
+  done
+  shift $(($OPTIND - 1))
 fi
